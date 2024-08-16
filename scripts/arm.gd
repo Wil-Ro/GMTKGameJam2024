8f22164693c7aa697055df583f5751335b458bf6
Rosia E Evans <willowro@outlook.com>
2024-08-17 00:21:49 +0100

    adds bones to arm

--- scripts/arm.gd
+++ scripts/arm.gd
@@ -7,6 +7,7 @@ class_name Arm extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$Skeleton3D/PhysicalBoneSimulator3D.physical_bones_start_simulation()
 	pass # Replace with function body.
 
 
